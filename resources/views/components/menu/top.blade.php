e15b6ea599a0a36b912eaf37b9a3bbcde5b519bc
Sergey Suprunov <s.suprunov@cargonomica.com>
2025-03-01 02:18:25 +0300

    menu assets

--- resources/views/components/menu/top.blade.php
+++ resources/views/components/menu/top.blade.php
@@ -2,168 +2,54 @@
     <ul>
         <li class="header__nav-item-sub">
             <div class="header__nav-label">Грузоперевозчикам
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none"
-                     viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 16 16">
                     <path stroke="#20212B" stroke-width="1.06667"
-                          d="m5.172 7.333 2.78 2.782a.067.067 0 0 0 .095 0l2.781-2.782"/>
+                          d="m5.172 7.333 2.78 2.782a.067.067 0 0 0 .095 0l2.781-2.782" />
                 </svg>
             </div>
             <div class="header__nav-sub">
                 <div class="header__nav-sub-item">
                     <div class="header__nav-sub-item-header">
-                        <a href="#" class="header__nav-sub-item-header-name">
-                            <img src="{{ Vite::image('header-pricep.svg') }}" alt="">
-                            полуприцеп.рф
-                        </a>
-                        <div class="header__nav-sub-item-header-label">полуприцепы с выгодой</div>
-                    </div>
-                    <ul>
-                        <li><a href="#">Аренда с выкупом тягачей</a></li>
-                        <li><a href="#">Шины в рассрочку</a></li>
-                        <li><a href="#">Сопутствующие товары</a></li>
-                        <li><a href="#">Трейд-ИН</a></li>
-                    </ul>
-                </div>
-                <div class="header__nav-sub-item">
-                    <div class="header__nav-sub-item-header">
-                        <a href="#" class="header__nav-sub-item-header-name">
-                            <img src="{{ Vite::image('header-used.svg') }}" alt="">
-                            Cargo.Used
-                        </a>
-                        <div class="header__nav-sub-item-header-label">полуприцепы с выгодой</div>
-                    </div>
-                    <ul>
-                        <li><a href="#">Аренда с выкупом тягачей</a></li>
-                        <li><a href="#">Шины в рассрочку</a></li>
-                        <li><a href="#">Сопутствующие товары</a></li>
-                        <li><a href="#">Трейд-ИН</a></li>
-                    </ul>
-                </div>
-                <div class="header__nav-sub-item">
-                    <div class="header__nav-sub-item-header">
-                        <a href="fuel.html" class="header__nav-sub-item-header-name">
+                        <a href="{{route('fuelIndex')}}" class="header__nav-sub-item-header-name">
                             <img src="{{ Vite::image('header-fuel.svg') }}" alt="">
                             Cargo.Fuel
                         </a>
                         <div class="header__nav-sub-item-header-label">скидка на топливо</div>
                     </div>
                     <ul>
-                        <li><a href="fuel-adblue.html">Adblue</a></li>
-                        <li><a href="#">Пропан</a></li>
-                        <li><a href="#">Метан</a></li>
-                        <li><a href="fuel-services.html">Сервисы</a></li>
-
-                    </ul>
-                </div>
-            </div>
-        </li>
-        <li class="header__nav-item-sub">
-            <div class="header__nav-label">Партнерам
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none"
-                     viewBox="0 0 16 16">
-                    <path stroke="#20212B" stroke-width="1.06667"
-                          d="m5.172 7.333 2.78 2.782a.067.067 0 0 0 .095 0l2.781-2.782"/>
-                </svg>
-            </div>
-            <div class="header__nav-sub">
-                <div class="header__nav-sub-item">
-                    <div class="header__nav-sub-item-header">
-                        <a href="#" class="header__nav-sub-item-header-name">
-                            <img src="{{ Vite::image('header-pricep.svg') }}" alt="">
-                            полуприцеп.рф
-                        </a>
-                        <div class="header__nav-sub-item-header-label">полуприцепы с выгодой</div>
-                    </div>
-                    <ul>
-                        <li><a href="#">Аренда с выкупом тягачей</a></li>
-                        <li><a href="#">Шины в рассрочку</a></li>
-                        <li><a href="#">Сопутствующие товары</a></li>
-                        <li><a href="#">Трейд-ИН</a></li>
-                    </ul>
-                </div>
-                <div class="header__nav-sub-item">
-                    <div class="header__nav-sub-item-header">
-                        <a href="#" class="header__nav-sub-item-header-name">
-                            <img src="{{ Vite::image('header-used.svg') }}" alt="">
-                            Cargo.Used
-                        </a>
-                        <div class="header__nav-sub-item-header-label">полуприцепы с выгодой</div>
-                    </div>
-                    <ul>
-                        <li><a href="#">Аренда с выкупом тягачей</a></li>
-                        <li><a href="#">Шины в рассрочку</a></li>
-                        <li><a href="#">Сопутствующие товары</a></li>
-                        <li><a href="#">Трейд-ИН</a></li>
+                        <li><a href="{{route('fuelAbout')}}">Топливо</a></li>
+                        <li><a href="{{route('fuelCards')}}">Топливные карты</a></li>
+                        <li><a href="{{route('fuelServices')}}">Сервисы общая</a></li>
+                        <li><a href="{{route('fuelAdblue')}}">Детальная сервиса</a></li>
+                        <li><a href="{{route('fuelRelatedServices')}}">Сопутствующие сервисы</a></li>
+                        <li><a href="{{route('fuelRelatedProducts')}}">Сопутствующие товары</a></li>
                     </ul>
                 </div>
             </div>
         </li>
         <li class="header__nav-item-sub">
             <div class="header__nav-label">Сотрудникам
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none"
-                     viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 16 16">
                     <path stroke="#20212B" stroke-width="1.06667"
-                          d="m5.172 7.333 2.78 2.782a.067.067 0 0 0 .095 0l2.781-2.782"/>
+                          d="m5.172 7.333 2.78 2.782a.067.067 0 0 0 .095 0l2.781-2.782" />
                 </svg>
             </div>
             <div class="header__nav-sub">
                 <div class="header__nav-sub-item">
                     <div class="header__nav-sub-item-header">
-                        <a href="#" class="header__nav-sub-item-header-name">
-                            <img src="{{ Vite::image('header-pricep.svg') }}" alt="">
-                            полуприцеп.рф
-                        </a>
-                        <div class="header__nav-sub-item-header-label">полуприцепы с выгодой</div>
+                        <a href="{{route('vacanciesIndex')}}" class="header__nav-sub-item-header-name"><img
+                                src="{{ Vite::image('header-fuel.svg') }}" alt="">Карьера </a>
+                        <div class="header__nav-sub-item-header-label">Работайте с нами</div>
                     </div>
                     <ul>
-                        <li><a href="#">Аренда с выкупом тягачей</a></li>
-                        <li><a href="#">Шины в рассрочку</a></li>
-                        <li><a href="#">Сопутствующие товары</a></li>
-                        <li><a href="#">Трейд-ИН</a></li>
+                        <li><a href="{{route('vacanciesVacancy', ['slug' => 'xxx'])}}">Вакансия</a></li>
                     </ul>
                 </div>
             </div>
         </li>
-        <li><a href="#">Инвесторам</a></li>
-
-        <li class="header__nav-item-much"><span>О&nbsp;компании</span></li>
-        <li class="header__nav-item-much"><a href="contacts.html">Контакты</a></li>
-
-        <li class="header__nav-item-sub header__nav-item-more">
-            <div class="header__nav-label">Еще
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none"
-                     viewBox="0 0 16 16">
-                    <path stroke="#20212B" stroke-width="1.06667"
-                          d="m5.172 7.333 2.78 2.782a.067.067 0 0 0 .095 0l2.781-2.782"/>
-                </svg>
-            </div>
-            <div class="header__nav-sub">
-                <div class="header__nav-sub-item _simple">
-                    <div class="header__nav-sub-item-header">
-                        <a href="about.html" class="header__nav-sub-item-header-name">
-                            О компании
-                        </a>
-                    </div>
-                    <ul>
-                        <li><a href="#">Подход</a></li>
-                        <li><a href="#">История</a></li>
-                        <li><a href="#">Структура</a></li>
-                    </ul>
-                </div>
-                <div class="header__nav-sub-item _simple">
-                    <div class="header__nav-sub-item-header">
-                        <a href="contacts.html" class="header__nav-sub-item-header-name">
-                            Контакты
-                        </a>
-                    </div>
-                    <ul>
-                        <li><a href="#">Сервисы</a></li>
-                        <li><a href="#">Отделы</a></li>
-                        <li><a href="#">Техподдержка</a></li>
-                        <li><a href="#">PR</a></li>
-                    </ul>
-                </div>
-            </div>
-        </li>
+        <li class="header__nav-item-much"><a href="{{route('aboutIndex')}}">О компании</a></li>
+        <li class="header__nav-item-much"><a href="{{route('contactsIndex')}}">Контакты</a></li>
+        <li class="header__nav-item-much"><a href="{{route('documentationIndex')}}">Документация</a></li>
+        <li class="header__nav-item-much"><a href="{{route('suppliersIndex')}}">Поставщикам</a></li>
     </ul>
 </nav>
